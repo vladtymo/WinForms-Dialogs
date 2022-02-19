f74e2d67d19ed6c3ae04244ce74b694eefe35227
vladtymo <tymo.vlad@gmail.com>
2022-02-19 10:04:18 +0200

    Added an example of Common Dialogs in WinForms.

---
+++ 02_Dialogs/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace _02_Dialogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(67, 193);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(1048, 97);
+            this.label.TabIndex = 0;
+            this.label.Text = "Lorme Ipsum dolor emet...";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 523);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(329, 113);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Change Font";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(477, 523);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(329, 113);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Change Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(873, 523);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(329, 113);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Load Text";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1266, 523);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(329, 113);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save Text";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(652, 687);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(370, 113);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Browse Folders";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1717, 876);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 02_Dialogs/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_Dialogs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label.Font = new Font("Times-New-Roman", 22);
+
+            FontDialog dialog = new FontDialog();
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+                label.Font = dialog.Font;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+                label.ForeColor = dialog.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //FileStream fs = new FileStream()
+            //StreamReader sr = new StreamReader()
+            //File.ReadAllText();
+
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            //dialog.Multiselect = true;
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string text = File.ReadAllText(dialog.FileName);
+                label.Text = text;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            dialog.DefaultExt = ".txt";
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                File.WriteAllText(dialog.FileName, label.Text);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                MessageBox.Show("Folder: " + dialog.SelectedPath);
+            }
+        }
+    }
+}
